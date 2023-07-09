dd821c438a44533404dd8f45fcfaf35e8aa2592b
proffapt <proffapt@pm.me>
2023-07-09 17:04:31 +0530

    feat: added login endpoints

---
+++ endpoints.py
@@ -0,0 +1,4 @@
+HOMEPAGE_URL = 'https://erp.iitkgp.ac.in/IIT_ERP3/welcome.jsp'
+LOGIN_URL = 'https://erp.iitkgp.ac.in/SSOAdministration/auth.htm'
+SECRET_QUESTION_URL = 'https://erp.iitkgp.ac.in/SSOAdministration/getSecurityQues.htm'
+OTP_URL = 'https://erp.iitkgp.ac.in/SSOAdministration/getEmilOTP.htm' # blame ERP for the typo
