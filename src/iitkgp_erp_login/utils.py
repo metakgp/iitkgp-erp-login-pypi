cde9ff7e18e0149a0af707eb31b82f3d647ad96f
proffapt <proffapt@pm.me>
2023-08-13 15:05:20 +0530

    feat: modularised writing and fetching of tokens from file

--- src/iitkgp_erp_login/utils.py
+++ src/iitkgp_erp_login/utils.py
@@ -5,7 +5,39 @@ from google.oauth2.credentials import Credentials
 from google.auth.transport.requests import Request
 from google_auth_oauthlib.flow import InstalledAppFlow
 
-def get_import_location(caller_file=None):
+import logging
+logging.basicConfig(level=logging.INFO)
+
+
+def write_tokens_to_file(token_file: str, sessionToken: str, ssoToken: str, log: bool):
+    """Writes session tokens to the token file if present."""
+    try:
+        with open(token_file, "w") as file:
+            file.write(sessionToken + "\n")
+            file.write(ssoToken + "\n")
+
+        if log: logging.info(" Stored tokens in file")
+    except IOError:
+        logging.error(" Error writing to token file %s", token_file)
+
+
+def get_tokens_from_file(token_file: str, log: bool):
+    """Reads session tokens from a token file if it exists."""
+    try:
+        with open(token_file, "r") as file:
+            lines = file.readlines()
+            sessionToken = lines[0].strip() if len(lines) > 0 else None
+            ssoToken = lines[1].strip() if len(lines) > 1 else None
+
+        if log: logging.info(" Retrieved tokens from file")
+    except (FileNotFoundError, IOError):
+        logging.error(f" Token file doesn't exist")
+
+    return sessionToken, ssoToken
+
+
+def get_import_location(caller_file: str = None):
+    """Gets the location of file where this module is imported."""
     if len(sys.argv) == 1 and sys.argv[0] == '-c':
         return os.getcwd()
     else:
@@ -21,7 +53,9 @@ def get_import_location(caller_file=None):
 
         return script_directory_path
 
+
 def generate_token():
+	"""Generates token.json from credentials.json file with readonly access to mails."""
 	if len(sys.argv) == 1 and sys.argv[0] == '-c':
 		caller_file = None
 	else:
