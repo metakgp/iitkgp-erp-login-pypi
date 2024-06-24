8e1ba6b37cb1b486616945ce98aacfa788c11e0f
proffapt <proffapt@pm.me>
2024-06-25 01:03:28 +0530

    feat: improved code quality

--- examples/server.py
+++ examples/server.py
@@ -1,4 +1,4 @@
-import webbrowser
+import logging
 from flask_cors import CORS
 from flask import Flask, request, jsonify
 from iitkgp_erp_login import session_manager
@@ -7,110 +7,152 @@ from iitkgp_erp_login import session_manager
 app = Flask(__name__)
 CORS(app)
 
-secret_key = "top-secret-unhackable-key"
+jwt_secret_key = "top-secret-unhackable-key"
 headers = {
     'timeout': '20',
     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/51.0.2704.79 Chrome/51.0.2704.79 Safari/537.36',
 }
 
-# Initialize the SessionManager instance
 session_manager = session_manager.SessionManager(
-    secret_key=secret_key, headers=headers)
+    jwt_secret_key=jwt_secret_key, headers=headers)
+
+
+class ErpResponse:
+    def __init__(self, success: bool, message: str = None, data: dict = None, status_code: int = 200):
+        self.success = success
+        self.message = message
+        self.data = data or {}
+        self.status_code = status_code
+
+        if not success:
+            logging.error(f" {message}")
+
+    def to_dict(self):
+        response = {
+            "status": "success" if self.success else "error",
+            "message": self.message
+        }
+        if self.data:
+            response.update(self.data)
+        return response
+
+    def to_response(self):
+        return jsonify(self.to_dict()), self.status_code
+
+
+def handle_auth() -> ErpResponse:
+    if "Authorization" in request.headers:
+        header = request.headers["Authorization"].split(" ")
+        if len(header) == 2:
+            return ErpResponse(True, data={
+                "jwt": header[1]
+            }).to_response()
+        else:
+            return ErpResponse(False, "Poorly formatted authorization header. Should be of format 'Bearer <token>'", status_code=401).to_response()
+    else:
+        return ErpResponse(False, "Authentication token not provided", status_code=401).to_response()
 
 
 @app.route("/secret-question", methods=["POST"])
 def get_secret_question():
     try:
         data = request.form
-
         roll_number = data.get("roll_number")
         if not roll_number:
-            return jsonify({"status": "error", "message": "Missing roll number"}), 400
+            return ErpResponse(False, "Roll Number not provided", status_code=400).to_response()
 
-        secret_question, token = session_manager.get_secret_question(
+        secret_question, jwt = session_manager.get_secret_question(
             roll_number)
-
-        return jsonify({"status": "success", "secret_question": secret_question, "token": token}), 200
+        return ErpResponse(True, data={
+            "secret_question": secret_question,
+            "jwt": jwt
+        }).to_response()
     except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+        return ErpResponse(False, str(e), status_code=500).to_response()
 
 
 @app.route("/request-otp", methods=["POST"])
 def request_otp():
     try:
-        token = None
-        if "Authorization" in request.headers:
-            token = request.headers["Authorization"].split(" ")[1]
-        if not token:
-            return jsonify({"status": "error", "message": "Authentication token not provided"}), 401
+        jwt = None
+        auth_resp, status_code = handle_auth()
+        if status_code != 200:
+            return auth_resp, status_code
+        else:
+            jwt = auth_resp.get_json().get("jwt")
 
         password = request.form.get("password")
         secret_answer = request.form.get("secret_answer")
+        if not all([password, secret_answer]):
+            return ErpResponse(False, "Missing password or secret answer", status_code=400).to_response()
 
-        if not all([token, password, secret_answer]):
-            return jsonify({"status": "error", "message": "Missing token, password, or secret answer"}), 400
-
-        session_manager.request_otp(token, password, secret_answer)
-        return jsonify({"status": "success", "message": "OTP sent"}), 200
+        session_manager.request_otp(jwt, password, secret_answer)
+        return ErpResponse(True, message="OTP has been sent to your connected email accounts").to_response()
     except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+        return ErpResponse(False, str(e), status_code=500).to_response()
 
 
 @app.route("/login", methods=["POST"])
 def login():
     try:
-        token = None
-        if "Authorization" in request.headers:
-            token = request.headers["Authorization"].split(" ")[1]
-        if not token:
-            return jsonify({"status": "error", "message": "Authentication token not provided"}), 401
+        jwt = None
+        auth_resp, status_code = handle_auth()
+        if status_code != 200:
+            return auth_resp, status_code
+        else:
+            jwt = auth_resp.get_json().get("jwt")
 
         password = request.form.get("password")
         secret_answer = request.form.get("secret_answer")
         otp = request.form.get("otp")
+        if not all([secret_answer, password, otp]):
+            return ErpResponse(False, "Missing password, secret answer or otp", status_code=400).to_response()
 
-        if not all([token, secret_answer, password, otp]):
-            return jsonify({"status": "error", "message": "Missing token or OTP"}), 400
-
-        session_manager.establish_erp_session(
-            token=token, answer=secret_answer, otp=otp, password=password)
-        return jsonify({"status": "success"}), 200
+        session_manager.login(jwt, password, secret_answer, otp)
+        return ErpResponse(True, message="Logged in to ERP").to_response()
     except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+        return ErpResponse(False, str(e), status_code=500).to_response()
 
 
 @app.route("/logout", methods=["GET"])
 def logout():
     try:
-        token = None
-        if "Authorization" in request.headers:
-            token = request.headers["Authorization"].split(" ")[1]
-        if not token:
-            return jsonify({"status": "error", "message": "Authentication token not provided"}), 401
+        jwt = None
+        auth_resp, status_code = handle_auth()
+        if status_code != 200:
+            return auth_resp, status_code
+        else:
+            jwt = auth_resp.get_json().get("jwt")
 
-        session_manager.end_session(token=token)
+        session_manager.end_session(jwt=jwt)
 
-        return jsonify({"status": "success", "message": "You are logged out"}), 200
+        return ErpResponse(True, message="Logged out of ERP").to_response()
     except Exception as e:
-        return jsonify({"status": "error", "message": str(e)}), 500
+        return ErpResponse(False, str(e), status_code=500).to_response()
 
 
 @app.route("/timetable", methods=["POST"])
 def timetable():
-    token = None
-    if "Authorization" in request.headers:
-        token = request.headers["Authorization"].split(" ")[1]
-    if not token:
-        return jsonify({"status": "error", "message": "Authentication token not provided"}), 401
+    try:
+        jwt = None
+        auth_resp, status_code = handle_auth()
+        if status_code != 200:
+            return auth_resp, status_code
+        else:
+            jwt = auth_resp.get_json().get("jwt")
 
-    _, ssoToken = session_manager.get_erp_session(token=token)
+        _, ssoToken = session_manager.get_erp_session(jwt=jwt)
 
-    ERP_TIMETABLE_URL = "https://erp.iitkgp.ac.in/Acad/student/view_stud_time_table.jsp"
-    data = {
-        "ssoToken": ssoToken,
-        "module_id": '16',
-        "menu_id": '40',
-    }
-    r = session_manager.request(
-        token=token, method='POST', url=ERP_TIMETABLE_URL, headers=headers, data=data)
-    return jsonify({"status_code": r.status_code}), 200
+        ERP_TIMETABLE_URL = "https://erp.iitkgp.ac.in/Acad/student/view_stud_time_table.jsp"
+        data = {
+            "ssoToken": ssoToken,
+            "module_id": '16',
+            "menu_id": '40',
+        }
+        r = session_manager.request(
+            jwt=jwt, method='POST', url=ERP_TIMETABLE_URL, headers=headers, data=data)
+        return ErpResponse(True, data={
+            "status_code": r.status_code
+        }).to_response()
+    except Exception as e:
+        return ErpResponse(False, str(e), status_code=500).to_response()
--- src/iitkgp_erp_login/session_manager.py
+++ src/iitkgp_erp_login/session_manager.py
@@ -1,16 +1,16 @@
-import jwt
 import requests
+import jwt as jwt_gen
 from threading import Timer
 import iitkgp_erp_login.erp as erp
 from typing import Dict, Literal, Tuple
 from datetime import datetime, timedelta
 
-# Times (in seconds)
+# In seconds
 SESSION_TIMEOUT = 60 * 30
 SESSION_CLEANUP = 60 * 60 * 6
 
 
-class Session:
+class UserSession:
     def __init__(self):
         self.erp_session = requests.Session()
         self.session_token = None
@@ -19,112 +19,106 @@ class Session:
 
 
 class SessionManager:
-    _instance = None
-    sessions: Dict[str, Session]
-
-    def __init__(self, headers: Dict[str, str], secret_key: str) -> None:
-        if not hasattr(self, 'initialized'):  # To ensure __init__ is called only once
-            self.sessions = {}
-            self.headers = headers
-            self.secret_key = secret_key
-            self.initialized = True
-            self.__cleanup_sessions()
+    def __init__(self, headers: Dict[str, str], jwt_secret_key: str) -> None:
+        self.user_sessions_dict: Dict[str, UserSession] = {}
+        self.headers = headers
+        self.jwt_secret_key = jwt_secret_key
+        self.__cleanup_sessions()
 
     def __generate_jwt(self, roll_number: str) -> str:
         payload = {
             'roll_number': roll_number,
             'exp': datetime.now() + timedelta(seconds=SESSION_TIMEOUT)  # Token expiration time
         }
-        token = jwt.encode(payload, self.secret_key, algorithm='HS256')
-        return token
+        jwt = jwt_gen.encode(payload, self.jwt_secret_key, algorithm='HS256')
+        return jwt
 
-    def __verify_jwt(self, token: str) -> str:
+    def __verify_jwt(self, jwt: str) -> str:
         try:
-            payload = jwt.decode(token, self.secret_key, algorithms=['HS256'])
+            payload = jwt_gen.decode(
+                jwt, self.jwt_secret_key, algorithms=['HS256'])
             return payload['roll_number']
-        except jwt.ExpiredSignatureError:
+        except jwt_gen.ExpiredSignatureError:
             raise ValueError("Token has expired")
-        except jwt.InvalidTokenError:
-            raise ValueError("Invalid token")
+        except jwt_gen.InvalidTokenError:
+            raise ValueError("Invalid jwt")
 
-    def __create_session(self, roll_number: str) -> Tuple[str, Session]:
-        token = self.__generate_jwt(roll_number)
-        session = Session()
-        self.sessions[token] = session
-        return token, session
+    def __create_session(self, roll_number: str) -> Tuple[str, UserSession]:
+        jwt = self.__generate_jwt(roll_number)
+        user_session = UserSession()
+        self.user_sessions_dict[jwt] = user_session
+        return jwt, user_session
 
-    def __get_session(self, token: str) -> Tuple[str, Session]:
-        roll_number = self.__verify_jwt(token)
-        session = self.sessions.get(token)
-        if not session:
-            raise ValueError(f"No ERP session found for token")
+    def __get_session(self, jwt: str) -> Tuple[str, UserSession]:
+        roll_number = self.__verify_jwt(jwt)
+        user_session = self.user_sessions_dict.get(jwt)
+        if not user_session:
+            raise ValueError(f"User was loggeed out. Log-in again")
 
         # Update the last accessed time
-        session.last_acccessed = datetime.now()
+        user_session.last_acccessed = datetime.now()
 
-        return roll_number, session
+        return roll_number, user_session
 
     def __cleanup_sessions(self) -> None:
-        now = datetime.now()
-        expired_sessions = [token for token, session in self.sessions.items()
-                            if now - session.last_acccessed > timedelta(seconds=SESSION_TIMEOUT)]
+        current_time = datetime.now()
+        expired_sessions = [jwt for jwt, session in self.user_sessions_dict.items()
+                            if current_time - session.last_acccessed > timedelta(seconds=SESSION_TIMEOUT)]
 
-        for token in expired_sessions:
-            self.end_session(token)
+        for jwt in expired_sessions:
+            self.end_session(jwt)
 
         Timer(SESSION_CLEANUP, self.__cleanup_sessions).start()
 
+    def get_erp_session(self, jwt: str) -> Tuple[str, str]:
+        _, user_session = self.__get_session(jwt)
+
+        return user_session.session_token, user_session.sso_token
+
+    def end_session(self, jwt: str) -> None:
+        if jwt in self.user_sessions_dict:
+            del self.user_sessions_dict[jwt]
+
     def get_secret_question(self, roll_number: str) -> Tuple[str, str]:
-        token, session = self.__create_session(roll_number)
+        jwt, user_session = self.__create_session(roll_number)
 
-        session_token = erp.get_sessiontoken(session.erp_session)
-        session.session_token = session_token
+        user_session.session_token = erp.get_sessiontoken(
+            user_session.erp_session)
         secret_question = erp.get_secret_question(
-            self.headers, session.erp_session, roll_number)
-        return secret_question, token
+            self.headers, user_session.erp_session, roll_number)
 
-    def request_otp(self, token: str, password: str, secret_answer: str) -> None:
-        roll_number, session = self.__get_session(token)
+        return secret_question, jwt
+
+    def request_otp(self, jwt: str, password: str, secret_answer: str) -> None:
+        roll_number, user_session = self.__get_session(jwt)
 
         login_details = erp.get_login_details(
-            roll_number, password, secret_answer, session.session_token)
+            roll_number, password, secret_answer, user_session.session_token)
 
-        erp.request_otp(self.headers, session.erp_session, login_details)
+        erp.request_otp(self.headers, user_session.erp_session, login_details)
 
-    def establish_erp_session(self, token: str, password: str, answer: str, otp: str) -> None:
-        roll_number, session = self.__get_session(token)
+    def login(self, jwt: str, password: str, secret_answer: str, otp: str) -> None:
+        roll_number, user_session = self.__get_session(jwt)
 
         login_details = erp.get_login_details(
             ROLL_NUMBER=roll_number,
             PASSWORD=password,
-            secret_answer=answer,
-            sessionToken=session.session_token
+            secret_answer=secret_answer,
+            sessionToken=user_session.session_token
         )
         login_details["email_otp"] = otp
 
-        sso_token = erp.signin(
-            self.headers, session.erp_session, login_details)
-        session.sso_token = sso_token
+        user_session.sso_token = erp.signin(
+            self.headers, user_session.erp_session, login_details)
 
-    def end_session(self, token: str) -> None:
-        if token in self.sessions:
-            del self.sessions[token]
+    def request(self, jwt: str, url: str, method: Literal['GET', 'POST', 'PUT', 'DELETE'] = 'GET', **kwargs) -> requests.Response:
+        _, user_session = self.__get_session(jwt)
 
-    def get_erp_session(self, token: str) -> Tuple[str, Session]:
-        _ = self.__verify_jwt(token)
-        session = self.sessions.get(token)
-        if not session:
-            raise ValueError(f"No ERP session found for token")
+        try:
+            response = user_session.erp_session.request(
+                method=method, url=url, **kwargs)
 
-        # Update the last accessed time
-        session.last_acccessed = datetime.now()
-
-        return session.session_token, session.sso_token
-
-    def request(self, token: str, url: str, method: Literal['GET', 'POST', 'PUT', 'DELETE'] = 'GET', **kwargs) -> requests.Response:
-        _, session = self.__get_session(token)
-
-        response = session.erp_session.request(
-            method=method, url=url, **kwargs)
-
-        return response
+            return response
+        except requests.exceptions.RequestException as e:
+            raise Exception(
+                f"Failed to request ERP endpoint ({url}): {str(e)}")
